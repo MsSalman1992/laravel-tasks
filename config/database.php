ba0301faf3a0554fcad86ba1dce08d00a8f4b156
MsSalman1992 <muhammad.salman1992@outlook.com>
2021-10-08 16:52:08 +0500

    test

--- config/database.php
+++ config/database.php
@@ -40,7 +40,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '10.0.0.7'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
